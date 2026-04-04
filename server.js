cd9283471ca0e723e8641952300a50e229174860
Ninsiima kevin <kevinninsiima3@gmail.com>
2026-04-04 22:08:47 +0300

    addition of base routes and connection to server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const routes = require('./routes');
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
@@ -8,5 +8,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send("API is running");
 });
+
+app.use('/', routes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
