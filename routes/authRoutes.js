59e50f067d6577296b0db06d67e15678ae62e166
Elimu Brian Joshua <bryanjelimu@gmail.com>
2026-04-08 00:14:26 +0300

    Update authRoutes.js
    Add login route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { register, login } = require('../controllers/authController');
+
+router.post('/login', login);
 const authController = require('../controllers/authController');
 
 router.post('/register', authController.register);
