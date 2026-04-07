917e496b47b4016075a9f451c8a7798ee8f3e52f
Ndiku Leonald <leonaldndiku@gmail.com>
2026-04-07 23:59:32 +0300

    add auth routes and connect to server

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const routes = require('./routes');
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
-
+const authRoutes = require('./routes/authRoutes');
 app.use(express.json());
+app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send("API is running");
