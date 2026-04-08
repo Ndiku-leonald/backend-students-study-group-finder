688a08a7d4980859c7fe7efb0b70c98e7ab25b1d
Ndiku Leonald <leonaldndiku@gmail.com>
2026-04-08 21:42:19 +0300

    add group routes for listing, search and join

--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/authMiddleware');
 const { createGroup } = require('../controllers/groupController');
+const { getGroups, searchGroups, joinGroup } = require('../controllers/groupController');
 
+router.get('/', getGroups);
+router.get('/search', searchGroups);
+router.post('/join/:groupId', auth, joinGroup);
 router.post('/create', auth, createGroup);
 
 module.exports = router;
