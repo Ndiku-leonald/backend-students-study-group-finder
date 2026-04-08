3939a7a0732ab65ae66db234fb36eded935271a9
unknown <komakechjob6@gmail.com>
2026-04-08 19:12:14 +0300

    Update groupController.js

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -1,12 +1,15 @@
 const Group = require('../models/Group');
+const GroupMember = require('../models/GroupMember');
 
 exports.createGroup = async (req, res) => {
   try {
+    if (!req.user?.id) return res.status(401).json({ error: 'Unauthorized' });
+    
     const group = await Group.create({
-      ...req.body,
+      name: req.body.name,
+      description: req.body.description,
       userId: req.user.id
     });
-
     res.json(group);
   } catch (error) {
     res.status(500).json({ error: error.message });
