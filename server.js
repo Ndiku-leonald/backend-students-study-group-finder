30a0e2b6402280625f8e69e2bc76bd601aab927b
Loku David Anthony <lokudavid45@gmail.com>
2026-04-11 12:15:07 +0300

    sever.js

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ const authRoutes = require('./routes/authRoutes');
 const groupRoutes = require('./routes/groupRoutes');
 const sessionRoutes = require('./routes/sessionRoutes');
 const favoriteRoutes = require('./routes/favoriteRoutes');
-
+const routes.post = require('./routes/postRoutes');
 app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/groups', groupRoutes);
 app.use('/sessions', sessionRoutes);
 app.use('/api/favorites', favoriteRoutes);
+app.use('/api/posts', postRoutes);
 
 app.get('/', (req, res) => {
   res.send("API is running");
